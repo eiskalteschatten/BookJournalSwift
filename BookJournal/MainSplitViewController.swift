a992ff5b6b0345f46fade7d8c34c111797434460
Alex <alex@alexseifert.com>
2018-04-02 11:00:03 +0200

    Added a NSSplitViewController class to control the main split view

---
+++ BookJournal/MainSplitViewController.swift
@@ -0,0 +1,23 @@
+//
+//  MainSplitViewController.swift
+//  BookJournal
+//
+//  Created by Alex Seifert on 02/04/2018.
+//  Copyright © 2018 Alex Seifert. All rights reserved.
+//
+
+import Cocoa
+
+class MainSplitViewController: NSSplitViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view.
+    }
+    
+    override var representedObject: Any? {
+        didSet {
+            // Update the view, if already loaded.
+        }
+    }
+}
