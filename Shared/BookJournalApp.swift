837248edb2ee134ab64951eb41f5dea30efbf2ed
Alex Seifert <alex@alexseifert.com>
2022-01-23 13:51:59 +0100

    Initial Commit

---
+++ Shared/BookJournalApp.swift
@@ -0,0 +1,20 @@
+//
+//  BookJournalApp.swift
+//  Shared
+//
+//  Created by Alex Seifert on 23.01.22.
+//
+
+import SwiftUI
+
+@main
+struct BookJournalApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
