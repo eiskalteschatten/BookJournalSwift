3669f0ba409e940445be0662f97c39ea8cf1e185
Alex Seifert <alex@alexseifert.com>
2022-02-20 21:03:17 +0100

    Add TODOs

--- Shared/Books/BookList.swift
+++ Shared/Books/BookList.swift
@@ -62,7 +62,7 @@ struct BookList: View {
             do {
                 try viewContext.save()
             } catch {
-                // Replace this implementation with code to handle the error appropriately.
+                // TODO: Replace this implementation with code to handle the error appropriately.
                 // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
                 let nsError = error as NSError
                 fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
@@ -77,7 +77,7 @@ struct BookList: View {
             do {
                 try viewContext.save()
             } catch {
-                // Replace this implementation with code to handle the error appropriately.
+                // TODO: Replace this implementation with code to handle the error appropriately.
                 // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
                 let nsError = error as NSError
                 fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
