63eadfa94d6255a0ff9f91a55eee7b7714079642
Alex Seifert <alex@alexseifert.com>
2022-02-22 21:08:57 +0100

    Move the focused bookcover

--- Shared/Books/BookView.swift
+++ Shared/Books/BookView.swift
@@ -15,43 +15,47 @@ struct BookView: View {
         VStack {
             if book != nil {
                 ScrollView {
-                    GeometryReader { geometry in
-                        if geometry.frame(in: .global).minY <= 0 {
+                    ZStack {
+                        GeometryReader { geometry in
+                            if geometry.frame(in: .global).minY <= 0 {
+                                Image("DefaultBookCover")
+                                    .resizable()
+                                    .scaledToFill()
+                                    .frame(width: geometry.size.width, height: geometry.size.height)
+                                    .offset(y: geometry.frame(in: .global).minY / 9)
+                                    .clipped()
+                                    .blur(radius: 30)
+                                    .opacity(0.4)
+                            }
+                            else {
+                                Image("DefaultBookCover")
+                                    .resizable()
+                                    .scaledToFill()
+                                    .frame(width: geometry.size.width, height: geometry.size.height + geometry.frame(in: .global).minY)
+                                    .clipped()
+                                    .offset(y: -geometry.frame(in: .global).minY)
+                                    .blur(radius: 30)
+                                    .opacity(0.4)
+                            }
+                        }
+                        .frame(height: 200)
+                        
+                        VStack(spacing: 30.0) {
+                            // TODO: add actual cover image
                             Image("DefaultBookCover")
                                 .resizable()
-                                .scaledToFill()
-                                .frame(width: geometry.size.width, height: geometry.size.height)
-                                .offset(y: geometry.frame(in: .global).minY / 9)
-                                .clipped()
-                                .blur(radius: 30)
-                                .opacity(0.4)
+                                .frame(width: 200.0, height: 307.0)
+                                .scaledToFit()
+                                .shadow(radius: 5)
+                            
+                            Text(book!.title!)
+                                .font(.title)
+                            
+                            if (book!.authors != nil) {
+                                Text(getBookAuthors(book!.authors!))
+                            }
                         }
-                        else {
-                            Image("DefaultBookCover")
-                                .resizable()
-                                .scaledToFill()
-                                .frame(width: geometry.size.width, height: geometry.size.height + geometry.frame(in: .global).minY)
-                                .clipped()
-                                .offset(y: -geometry.frame(in: .global).minY)
-                                .blur(radius: 30)
-                                .opacity(0.4)
-                        }
-                    }
-                    .frame(height: 400)
-                    
-                    VStack(spacing: 30.0) {
-                        // TODO: add actual cover image
-                        Image("DefaultBookCover")
-                            .resizable()
-                            .frame(width: 200.0, height: 307.0)
-                            .scaledToFit()
-                        
-                        Text(book!.title!)
-                            .font(.title)
-                        
-                        if (book!.authors != nil) {
-                            Text(getBookAuthors(book!.authors!))
-                        }
+                        .offset(y: 100.0)
                     }
                 }
                 #if os(iOS)
@@ -94,6 +98,9 @@ struct BookView_Previews: PreviewProvider {
     static var previews: some View {
         let context = PersistenceController.preview.container.viewContext
         let book = Book(context: context)
-        BookView(book: book).padding(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/).environment(\.managedObjectContext, context)
+        Group {
+            BookView(book: book).preferredColorScheme(.dark).padding(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/).frame(height: /*@START_MENU_TOKEN@*/800.0/*@END_MENU_TOKEN@*/).environment(\.managedObjectContext, context)
+            BookView(book: book).preferredColorScheme(.light).padding(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/).frame(height: 800.0).environment(\.managedObjectContext, context)
+        }
     }
 }
