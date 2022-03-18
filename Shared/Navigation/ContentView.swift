b602f5b689632af25f7ed186e7e03702642fccfe
Alex Seifert <alex@alexseifert.com>
2022-03-18 12:24:21 +0100

    Fix a bug preventing a window from opening on Mac

--- Shared/Navigation/ContentView.swift
+++ Shared/Navigation/ContentView.swift
@@ -38,7 +38,7 @@ struct ContentView: View {
                 )
                 NavigationLink(
                     destination: BookList(
-                        predicate: NSPredicate(format: "status == %@", BookReadingStatus.currentlyReading.rawValue)
+                        predicate: NSPredicate(format: "readingStatus == %@", BookReadingStatus.currentlyReading.rawValue)
                     ).navigationTitle("Currently Reading"),
                     tag: Screen.currentlyReading,
                     selection: $screen,
@@ -48,7 +48,7 @@ struct ContentView: View {
                 )
                 NavigationLink(
                     destination: BookList(
-                        predicate: NSPredicate(format: "status == %@", BookReadingStatus.notReadYet.rawValue)
+                        predicate: NSPredicate(format: "readingStatus == %@", BookReadingStatus.notReadYet.rawValue)
                     ).navigationTitle("Not Read Yet"),
                     tag: Screen.notReadYet,
                     selection: $screen,
@@ -58,7 +58,7 @@ struct ContentView: View {
                 )
                 NavigationLink(
                     destination: BookList(
-                        predicate: NSPredicate(format: "status == %@", BookReadingStatus.read.rawValue)
+                          predicate: NSPredicate(format: "readingStatus == %@", BookReadingStatus.read.rawValue)
                     ).navigationTitle("Books Read"),
                     tag: Screen.read,
                     selection: $screen,
