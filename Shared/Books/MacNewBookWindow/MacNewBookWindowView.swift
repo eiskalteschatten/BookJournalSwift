99a8250496792423c962b42b7065a0fc27d3aaf1
Alex Seifert <alex@alexseifert.com>
2022-03-20 19:23:11 +0100

    Style the cancel and save buttons

--- Shared/Books/MacNewBookWindow/MacNewBookWindowView.swift
+++ Shared/Books/MacNewBookWindow/MacNewBookWindowView.swift
@@ -16,206 +16,214 @@ struct MacNewBookWindowView: View {
     @StateObject private var newBookFormModel = NewBookFormModel()
     
     var body: some View {
-        HStack {
-            VStack {
-                // Bookcover
-                Image(systemName: "plus.square.dashed")
-                    .font(.system(size: 150))
-                    .foregroundColor(.accentColor)
-                
-                Button {
-                    // TODO: add function
-                } label: {
-                    Text("Choose Image")
+        VStack(alignment: .trailing) {
+            HStack {
+                VStack {
+                    // Bookcover
+                    Image(systemName: "plus.square.dashed")
+                        .font(.system(size: 150))
+                        .foregroundColor(.accentColor)
+                    
+                    Button {
+                        // TODO: add function
+                    } label: {
+                        Text("Choose Image")
+                    }
+                    
+                    Button {
+                        // TODO: add function
+                    } label: {
+                        Text("Scan Image")
+                    }
                 }
+                .padding(.trailing, 25)
                 
-                Button {
-                    // TODO: add function
-                } label: {
-                    Text("Scan Image")
+                Form {
+                    Group {
+                        // Title
+                        TextField(
+                            "Title:",
+                            text: $newBookFormModel.title
+                        )
+                        
+                        // Reading Status
+                        Picker("Reading Status:", selection: $newBookFormModel.readingStatus) {
+                            ForEach(BookReadingStatus.allCases) { status in
+                                Text(bookReadingStatusProperties[status]!)
+                                    .tag(status.rawValue)
+                            }
+                        }
+                        
+                        // Date Started
+                        if !newBookFormModel.addDateStarted {
+                            Button("Add Date Started") {
+                                newBookFormModel.addDateStarted.toggle()
+                            }
+                        }
+                        else {
+                            HStack {
+                                DatePicker(selection: $newBookFormModel.dateStarted, displayedComponents: .date) {
+                                    Text("Date Started:")
+                                }
+                                
+                                Button {
+                                    newBookFormModel.addDateStarted.toggle()
+                                } label: {
+                                    Image(systemName: "xmark.circle")
+                                        .foregroundColor(.red)
+                                }
+                                .padding(.leading, 5)
+                            }
+                        }
+                        
+                        // Date Finished
+                        if !newBookFormModel.addDateFinished {
+                            Button("Add Date Finished") {
+                                newBookFormModel.addDateFinished.toggle()
+                            }
+                        }
+                        else {
+                            HStack {
+                                DatePicker(selection: $newBookFormModel.dateFinished, displayedComponents: .date) {
+                                    Text("Date Finished:")
+                                }
+                                
+                                Button {
+                                    newBookFormModel.addDateFinished.toggle()
+                                } label: {
+                                    Image(systemName: "xmark.circle")
+                                        .foregroundColor(.red)
+                                }
+                                .padding(.leading, 5)
+                            }
+                        }
+                    }
+                        
+        //            // People
+        //            Group {
+        //                // Authors
+        //                NavigationLink(
+        //                    destination: AuthorsSearchList(selectedItems: $authors),
+        //                    tag: Screen.addAuthors,
+        //                    selection: $screen,
+        //                    label: { WrappingSmallChipsWithName<Author>(title: "Authors", data: authors, chipColor: AUTHOR_COLOR) }
+        //                )
+        //
+        //                // Editors
+        //                NavigationLink(
+        //                    destination: EditorsSearchList(selectedItems: $editors),
+        //                    tag: Screen.addEditors,
+        //                    selection: $screen,
+        //                    label: { WrappingSmallChipsWithName<Editor>(title: "Editors", data: editors, chipColor: EDITOR_COLOR) }
+        //                )
+        //            }
+                        
+                    // Book Information
+                    Group {
+                        // Page Count
+                        TextField(
+                            "Page Count:",
+                            value: $newBookFormModel.pageCount,
+                            format: .number
+                        )
+                            
+        //                // Genres
+        //                NavigationLink(
+        //                    destination: GenresSearchList(selectedItems: $genres),
+        //                    tag: Screen.addGenres,
+        //                    selection: $screen,
+        //                    label: { WrappingSmallChipsWithName<Genre>(title: "Genres", data: genres, chipColor: GENRE_COLOR) }
+        //                )
+        //
+        //                // Categories
+        //                NavigationLink(
+        //                    destination: CategoriesSearchList(selectedItems: $categories),
+        //                    tag: Screen.addCategories,
+        //                    selection: $screen,
+        //                    label: { WrappingSmallChipsWithName<Category>(title: "Categories", data: categories, chipColor: CATEGORY_COLOR) }
+        //                )
+        //
+        //                // Tags
+        //                NavigationLink(
+        //                    destination: TagsSearchList(selectedItems: $tags),
+        //                    tag: Screen.addTags,
+        //                    selection: $screen,
+        //                    label: { WrappingSmallChipsWithName<Tag>(title: "Tags", data: tags, chipColor: TAG_COLOR) }
+        //                )
+                        }
+                        
+                    Group {
+                        // Book Format
+                        Picker("Book Format:", selection: $newBookFormModel.bookFormat) {
+                            ForEach(BookFormat.allCases) { format in
+                                Label(bookFormatProperties[format]![0], systemImage: bookFormatProperties[format]![1])
+                                    .tag(format.rawValue)
+                            }
+                        }
+                            
+        //                // Publisher
+        //                NavigationLink(
+        //                    destination: PublishersSearchList(selectedItem: $publisher),
+        //                    tag: Screen.addPublisher,
+        //                    selection: $screen,
+        //                    label: { PickerMimickerWithName<Publisher>(title: "Publisher", data: publisher) }
+        //                )
+                            
+                        // Year Published
+                        TextField(
+                            "Year Published:",
+                            value: $newBookFormModel.yearPublished,
+                            format: .number
+                        )
+                        
+                        // ISBN
+                        TextField(
+                            "ISBN:",
+                            text: $newBookFormModel.isbn
+                        )
+                    }
+                        
+                    // World
+        //            Group {
+        //                // Country of Origin
+        //                NavigationLink(
+        //                    destination: CountriesSearchList(selectedItem: $countryOfOrigin),
+        //                    tag: Screen.addCountryOfOrigin,
+        //                    selection: $screen,
+        //                    label: { PickerMimickerWithName<Country>(title: "Country of Origin", data: countryOfOrigin) }
+        //                )
+        //
+        //                // Translators
+        //                NavigationLink(
+        //                    destination: TranslatorsSearchList(selectedItems: $translators),
+        //                    tag: Screen.addTranslators,
+        //                    selection: $screen,
+        //                    label: { WrappingSmallChipsWithName<Translator>(title: "Translators", data: translators, chipColor: TRANSLATOR_COLOR) }
+        //                )
+        //
+        //                LanguagePicker(title: "Original Language", selection: $originalLanguage)
+        //                LanguagePicker(title: "Language Read In", selection: $languageReadIn)
+        //            }
+                    
                 }
             }
-            .padding(.trailing, 25)
+            .padding(.bottom, 15)
             
-            Form {
-                Group {
-                    // Title
-                    TextField(
-                        "Title:",
-                        text: $newBookFormModel.title
-                    )
-                    
-                    // Reading Status
-                    Picker("Reading Status:", selection: $newBookFormModel.readingStatus) {
-                        ForEach(BookReadingStatus.allCases) { status in
-                            Text(bookReadingStatusProperties[status]!)
-                                .tag(status.rawValue)
-                        }
-                    }
-                    
-                    // Date Started
-                    if !newBookFormModel.addDateStarted {
-                        Button("Add Date Started") {
-                            newBookFormModel.addDateStarted.toggle()
-                        }
-                    }
-                    else {
-                        HStack {
-                            DatePicker(selection: $newBookFormModel.dateStarted, displayedComponents: .date) {
-                                Text("Date Started:")
-                            }
-                            
-                            Button {
-                                newBookFormModel.addDateStarted.toggle()
-                            } label: {
-                                Image(systemName: "xmark.circle")
-                                    .foregroundColor(.red)
-                            }
-                            .padding(.leading, 5)
-                        }
-                    }
-                    
-                    // Date Finished
-                    if !newBookFormModel.addDateFinished {
-                        Button("Add Date Finished") {
-                            newBookFormModel.addDateFinished.toggle()
-                        }
-                    }
-                    else {
-                        HStack {
-                            DatePicker(selection: $newBookFormModel.dateFinished, displayedComponents: .date) {
-                                Text("Date Finished:")
-                            }
-                            
-                            Button {
-                                newBookFormModel.addDateFinished.toggle()
-                            } label: {
-                                Image(systemName: "xmark.circle")
-                                    .foregroundColor(.red)
-                            }
-                            .padding(.leading, 5)
-                        }
-                    }
-                }
-                    
-    //            // People
-    //            Group {
-    //                // Authors
-    //                NavigationLink(
-    //                    destination: AuthorsSearchList(selectedItems: $authors),
-    //                    tag: Screen.addAuthors,
-    //                    selection: $screen,
-    //                    label: { WrappingSmallChipsWithName<Author>(title: "Authors", data: authors, chipColor: AUTHOR_COLOR) }
-    //                )
-    //
-    //                // Editors
-    //                NavigationLink(
-    //                    destination: EditorsSearchList(selectedItems: $editors),
-    //                    tag: Screen.addEditors,
-    //                    selection: $screen,
-    //                    label: { WrappingSmallChipsWithName<Editor>(title: "Editors", data: editors, chipColor: EDITOR_COLOR) }
-    //                )
-    //            }
-                    
-                // Book Information
-                Group {
-                    // Page Count
-                    TextField(
-                        "Page Count:",
-                        value: $newBookFormModel.pageCount,
-                        format: .number
-                    )
-                        
-    //                // Genres
-    //                NavigationLink(
-    //                    destination: GenresSearchList(selectedItems: $genres),
-    //                    tag: Screen.addGenres,
-    //                    selection: $screen,
-    //                    label: { WrappingSmallChipsWithName<Genre>(title: "Genres", data: genres, chipColor: GENRE_COLOR) }
-    //                )
-    //
-    //                // Categories
-    //                NavigationLink(
-    //                    destination: CategoriesSearchList(selectedItems: $categories),
-    //                    tag: Screen.addCategories,
-    //                    selection: $screen,
-    //                    label: { WrappingSmallChipsWithName<Category>(title: "Categories", data: categories, chipColor: CATEGORY_COLOR) }
-    //                )
-    //
-    //                // Tags
-    //                NavigationLink(
-    //                    destination: TagsSearchList(selectedItems: $tags),
-    //                    tag: Screen.addTags,
-    //                    selection: $screen,
-    //                    label: { WrappingSmallChipsWithName<Tag>(title: "Tags", data: tags, chipColor: TAG_COLOR) }
-    //                )
-                    }
-                    
-                Group {
-                    // Book Format
-                    Picker("Book Format:", selection: $newBookFormModel.bookFormat) {
-                        ForEach(BookFormat.allCases) { format in
-                            Label(bookFormatProperties[format]![0], systemImage: bookFormatProperties[format]![1])
-                                .tag(format.rawValue)
-                        }
-                    }
-                        
-    //                // Publisher
-    //                NavigationLink(
-    //                    destination: PublishersSearchList(selectedItem: $publisher),
-    //                    tag: Screen.addPublisher,
-    //                    selection: $screen,
-    //                    label: { PickerMimickerWithName<Publisher>(title: "Publisher", data: publisher) }
-    //                )
-                        
-                    // Year Published
-                    TextField(
-                        "Year Published:",
-                        value: $newBookFormModel.yearPublished,
-                        format: .number
-                    )
-                    
-                    // ISBN
-                    TextField(
-                        "ISBN:",
-                        text: $newBookFormModel.isbn
-                    )
-                }
-                    
-                // World
-    //            Group {
-    //                // Country of Origin
-    //                NavigationLink(
-    //                    destination: CountriesSearchList(selectedItem: $countryOfOrigin),
-    //                    tag: Screen.addCountryOfOrigin,
-    //                    selection: $screen,
-    //                    label: { PickerMimickerWithName<Country>(title: "Country of Origin", data: countryOfOrigin) }
-    //                )
-    //
-    //                // Translators
-    //                NavigationLink(
-    //                    destination: TranslatorsSearchList(selectedItems: $translators),
-    //                    tag: Screen.addTranslators,
-    //                    selection: $screen,
-    //                    label: { WrappingSmallChipsWithName<Translator>(title: "Translators", data: translators, chipColor: TRANSLATOR_COLOR) }
-    //                )
-    //
-    //                LanguagePicker(title: "Original Language", selection: $originalLanguage)
-    //                LanguagePicker(title: "Language Read In", selection: $languageReadIn)
-    //            }
+            HStack {
+                Button("Cancel", action: {
+                    NSApplication.shared.keyWindow?.close()
+                })
+                .keyboardShortcut(.cancelAction)
                 
-                HStack {
-                    Button("Cancel", action: {
-                        NSApplication.shared.keyWindow?.close()
-                    })
-                    Button("Save", action: {
-                        newBookFormModel.saveBook()
-                        NSApplication.shared.keyWindow?.close()
-                    })
-                }
+                Button("Save", action: {
+                    newBookFormModel.saveBook()
+                    NSApplication.shared.keyWindow?.close()
+                })
+                .keyboardShortcut(.defaultAction)
             }
         }
-        .padding(15)
+        .padding(.horizontal, 15)
+        .padding(.leading, 15)
         .frame(minWidth: 700)
     }
 }
