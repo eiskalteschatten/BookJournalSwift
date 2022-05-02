0501cc6b5a434bd0f24b20f374b17ce37fc6dd94
Alex Seifert <alex@alexseifert.com>
2022-05-02 10:36:23 +0200

    Move the sidebar to its own view

---
+++ Shared/Navigation/Sidebar.swift
@@ -0,0 +1,95 @@
+//
+//  Sidebar.swift
+//  BookJournal
+//
+//  Created by Alex Seifert on 02.05.22.
+//
+
+import SwiftUI
+
+struct Sidebar: View {
+    private enum Screen: Int {
+        case allBooks, wishlist, currentlyReading, notReadYet, read, statistics
+    }
+    
+    @SceneStorage("Sidebar.screen") private var screen: Screen?
+    
+    var body: some View {
+        List {
+            Section("Library") {
+                NavigationLink(
+                    destination: BookList().navigationTitle("All Books"),
+                    tag: Screen.allBooks,
+                    selection: $screen,
+                    label: {
+                        Label("All Books", systemImage: "books.vertical")
+                    }
+                )
+                NavigationLink(
+                    destination: BookList(
+                        predicate: NSPredicate(format: "readingStatus == %@", BookReadingStatus.currentlyReading.rawValue),
+                        createOptions: BookModelCreateOptions(readingStatus: .currentlyReading)
+                    ).navigationTitle("Currently Reading"),
+                    tag: Screen.currentlyReading,
+                    selection: $screen,
+                    label: {
+                        Label("Currently Reading", systemImage: "book")
+                    }
+                )
+                NavigationLink(
+                    destination: BookList(
+                        predicate: NSPredicate(format: "readingStatus == %@", BookReadingStatus.notReadYet.rawValue),
+                        createOptions: BookModelCreateOptions(readingStatus: .notReadYet)
+                    ).navigationTitle("Not Read Yet"),
+                    tag: Screen.notReadYet,
+                    selection: $screen,
+                    label: {
+                        Label("Not Read Yet", systemImage: "book.closed")
+                    }
+                )
+                NavigationLink(
+                    destination: BookList(
+                          predicate: NSPredicate(format: "readingStatus == %@", BookReadingStatus.read.rawValue),
+                          createOptions: BookModelCreateOptions(readingStatus: .read)
+                    ).navigationTitle("Books Read"),
+                    tag: Screen.read,
+                    selection: $screen,
+                    label: {
+                        Label("Books Read", systemImage: "checkmark.square")
+                    }
+                )
+            }
+            
+            Section("Lists") {
+                NavigationLink(
+                    destination: BookList(
+                        predicate: NSPredicate(format: "onWishlist == true"),
+                        createOptions: BookModelCreateOptions(onWishlist: true)
+                    ).navigationTitle("Wishlist"),
+                    tag: Screen.wishlist,
+                    selection: $screen,
+                    label: {
+                        Label("Wishlist", systemImage: "list.star")
+                    }
+                )
+            }
+            
+//                Section("Statistics") {
+//                    NavigationLink(
+//                        destination: BookList().navigationTitle("Statistics"),
+//                        tag: Screen.statistics,
+//                        selection: $screen,
+//                        label: {
+//                            Label("Statistics", systemImage: "chart.bar")
+//                        }
+//                    )
+//                }
+        }
+    }
+}
+
+struct Sidebar_Previews: PreviewProvider {
+    static var previews: some View {
+        Sidebar()
+    }
+}
